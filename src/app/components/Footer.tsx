7bd25e33c08840d8d6512c773e41fe811ef70395
Fabiano Moraes <32004353+masternetfmoraes@users.noreply.github.com>
2023-08-07 10:47:08 -0300

    maps

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer(){
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <a href="#" className="hover:underline">R. Cícero Ramos Meira, 186 - Jardim Nossa Sra. de Fatima, Hortolândia - SP, 13185-432</a>
+                    <a href="https://www.google.com/maps/dir/-23.9626867,-46.4135354/casa+de+repouso+bem+estar/@-23.4267715,-47.460042,9z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x94c8b98511613355:0x3d893f69a8d23526!2m2!1d-47.1638268!2d-22.8941464?entry=ttu" className="hover:underline">R. Cícero Ramos Meira, 186 - Jardim Nossa Sra. de Fatima, Hortolândia - SP, 13185-432</a>
                 </li>
             </ul>
         </footer>
