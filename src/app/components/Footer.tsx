d4b4d5f51b572b40f766aabc6cdfda9ffa343e8b
Fabiano Moraes <32004353+masternetfmoraes@users.noreply.github.com>
2023-08-07 10:49:29 -0300

    Atizapi

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return(
         <>
         <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Casa de Repouso Bem Estar™</a>. (19) 3887-3384.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Casa de Repouso Bem Estar™</a>. <a href="https://wa.me/551938873384">(19) 3887-3384</a>.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
